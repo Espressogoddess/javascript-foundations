c03c228a9927e71174dfc7ed1411ccea5a294213
Amber Shipley <ashipley2@me.com>
2022-12-05 15:33:48 -0800

    Complete unicorn function and unskip tests

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,22 @@
 class Unicorn {
   constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    this.name = name
+    this.color = color
+    if (color === undefined) {
+      this.color = 'white'
+    }
   }
+  isWhite() {
+    if (this.color === undefined || this.color !== 'white') {
+    return false
+    } else {
+      return true
+    }
+  }
+  says(phrase) {
+    return `**;* ${phrase} *;**`
+  }
+
 }
 
 module.exports = Unicorn;
