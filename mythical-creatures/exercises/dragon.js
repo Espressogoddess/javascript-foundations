9f6006b3fd0cbd2f8a0028a819db05ba21eaf6e0
Amber Shipley <ashipley2@me.com>
2022-12-12 14:02:52 -0800

    Unskip and pass tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+    constructor(dragonName, riderName) {
+        this.name = dragonName
+        this.rider = riderName
+        this.hungry = true
+        this.count = 0
+    }
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+    eat() {
+        this.count++
+        if (this.count >= 3) {
+            this.hungry = false
+            }
+    }
+}
 
+module.exports = Dragon
