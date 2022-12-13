2d234cde598d8ded029b6e142b514465c807ab36
Amber Shipley <ashipley2@me.com>
2022-12-13 11:50:32 -0800

    Unskip and pass tests and update completed mythical creatures

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -8,6 +8,12 @@ class Medusa {
     }
     gazeAtVictim(victim) {
         this.statues.push(new Statue(victim.name));
+        if (this.statues.length > 3) {
+            var backToPerson = this.statues.shift();
+            var newPerson = new Person(backToPerson.name);
+            newPerson.mood = 'relieved';
+            return newPerson;
+        }
     }
 }
 
