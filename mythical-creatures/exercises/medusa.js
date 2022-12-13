d9bf9e815856207cd0250d7bd26ea7871d4640f7
Amber Shipley <ashipley2@me.com>
2022-12-12 18:51:56 -0800

    Begin working on passing medusa tests, which requires beginning person and statue files

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,14 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(person) {
+        this.name = person;
+        this.statues = [];
+    }
+    gazeAtVictim(victim) {
+        this.statues.push(new Statue(victim.name));
+    }
+}
+
+module.exports = Medusa;
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,7 @@
+class Person {
+    constructor(person) {
+        this.name = person
+        this.mood = 'frightened'
+    }
+}
+module.exports = Person;
