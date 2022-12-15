c4ec2ab4593edb9f08212265ed0b94b0fca1a01f
Amber Shipley <ashipley2@me.com>
2022-12-14 19:00:14 -0800

    Unskip and pass tests, as well as, update mythical creatures completed.

---
+++ mythical-creatures/exercises/victim.js
@@ -0,0 +1,9 @@
+class Victim {
+    constructor(person) {
+        this.name = person;
+        this.alive = true;
+    }
+
+}
+
+module.exports = Victim;
---
+++ mythical-creatures/exercises/werewolf.js
@@ -0,0 +1,29 @@
+class Werewolf {
+    constructor(werewolfName) {
+        this.name = werewolfName;
+        this.form = 'human';
+        this.hungry = false;
+    }
+    completeTransformation() {
+        if (this.form === 'human') {
+        this.form = 'wolf';
+        this.hungry = true;
+        return 'Aaa-Woooo!';
+        } else {
+            this.form = 'human';
+            this.hungry = false;
+            return 'Where are I?';
+        }
+    }
+    eatVictim(victim) {
+        if (this.form === 'human') {
+            return 'No way am I eating Baby, I\'d like a burger!';
+        } else {
+        victim.alive = false;
+        this.form = 'human';
+        return 'Yum, Hannah was delicious.';
+        }
+    }
+}
+
+module.exports = Werewolf;
