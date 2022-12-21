b82cf9654d580634855c4a9286843b6273933468
Amber Shipley <ashipley2@me.com>
2022-12-21 12:44:26 -0800

    Unskip and pass craft and material tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,21 @@
 class Craft {
+    constructor(info) {
+        this.name = info.type;
+        this.materials = info.materials;
+        this.completed = false;
 
+    }
+    completeCraft() {
+        this.completed = true;
+        return 'All done! It looks great!'
+    }
+    calculateProjectTotal() {
+        var totalCost = 0;
+        for (var i = 0; i <this.materials.length; i++) {
+            totalCost += this.materials[i].price * this.materials[i].amount;
+        }
+        return totalCost;
+    }
 }
 
 module.exports = Craft;
