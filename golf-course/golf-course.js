d2c2443d21ee7050de6832328946749b220e563e
Amber Shipley <ashipley2@me.com>
2022-12-20 19:00:08 -0800

    Make all tests pass

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,23 @@
 class GolfCourse {
-
-
+    constructor(courseName, diffRating, openingDays, features) {
+        this.name = courseName;
+        this.difficulty = diffRating;
+        this.openings = openingDays;
+        this.features = features;
+        this.currentlyPlaying = [];
+    }
+    checkInGroup(group) { 
+        if (this.openings >= group.length) {
+            for (var i = 0; i < group.length; i++) {
+                this.currentlyPlaying.unshift(group[i].name);
+            }
+            this.openings -= group.length;
+            return 'You\'re checked in. Have fun!';
+        } else {
+            return 'Sorry, we are currently booked! Please come back later.'
+        }
+        
+    }
 }
 
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,42 @@
 class Golfer {
-
-
+    constructor(personInfo) {
+        this.name = personInfo.name;
+        this.handicap = personInfo.handicap
+        this.frustration = 0;
+        this.confidence = 0;
+    }
+    calculateAvg(par) {
+        var avgPar = par + this.handicap;
+        return `I usually shoot a ${avgPar} on average.`
+    } 
+    playRound(courseInfo) {
+        if (courseInfo.difficulty === 'hard') {
+            this.frustration = 500;
+        }
+        if (courseInfo.difficulty === 'moderate') {
+            this.frustration = 100;
+        }
+    }
+    hitTheRange() {
+        this.confidence += 10;
+    }
+    whatYaShoot(num) {
+        if (num > 0) {
+            this.frustration += 20;
+            return 'Doh!';
+        }
+        if (num === 0) {
+            this.frustration -= 10;
+            return 'Booyah!';
+        }
+        if (num < 0) {
+            this.frustration = 0;
+            return 'I AM THE GREATEST GOLFER ALIVE!';
+        }
+    }
+    marvel(courseInfo) {
+        return `I love the ${courseInfo.features[0]} and ${courseInfo.features[1]} on this course!`;
+    }
 }
 
 module.exports = Golfer;
