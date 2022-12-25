c21cedf7a977762ea9c5127396f0c1c48d1c666c
Amber Shipley <ashipley2@me.com>
2022-12-25 15:27:40 -0800

    Implement and unskip tests for parts

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,20 @@
 class Part {
+    constructor(part) {
+        this.type = part;
+        this.broken = false;
+        this.count = 0;
+    }
 
+    break() {
+        this.broken = true;
+        this.count++;
+    }
+    repair() {
+        if (this.count > 1) {
+            return `This ${this.type} has seen better days, we\'ll need a brand new one!`;
+        }
+        this.broken = false;
+    }
 }
 
 module.exports = Part;
