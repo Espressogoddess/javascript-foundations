f41bc67cc5def70656f541863d277ec770cd65cc
Amber Shipley <ashipley2@me.com>
2022-12-25 13:04:21 -0800

    Fully implement sphinx tests and update creatures completed

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -2,6 +2,7 @@ class Sphinx {
     constructor() {
         this.name = null;
         this.riddles = [];    
+        this.heroesEaten = 0;
     }
     collectRiddle(riddle) {
         if (this.riddles.length < 3) {
@@ -11,9 +12,20 @@ class Sphinx {
             this.riddles.shift();
             this.riddles.push(riddle);
         }
-
     }
-    
+    attemptAnswer(answer) {
+        for (var i = 0; i < this.riddles.length; i++) {
+            if (answer === this.riddles[i].answer) {
+                this.riddles.splice(i, 1);
+                if (this.riddles.length === 0) {
+                    return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answer}"???`
+                } else {
+                    return 'That wasn\'t that hard, I bet you don\'t get the next one'
+                }
+            }
+        }
+        this.heroesEaten++;
+    }
 }
 
 module.exports = Sphinx;
