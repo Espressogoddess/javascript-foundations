9718e578ab43fba1542c7f7bc1a830dd0dac6d2a
Amber Shipley <ashipley2@me.com>
2022-12-25 19:50:28 -0800

    Partially implement wagon and unskip tests

---
+++ oregon-trail/src/wagon.js
@@ -0,0 +1,16 @@
+class Wagon {
+    constructor(wagon) {
+        this.title = wagon.name;
+        this.wheels = wagon.wheels || [];
+        this.axles = wagon.axles || [];
+        this.oxen = wagon.oxen || [];
+        this.yokes = wagon.yokes || [];
+        this.food = wagon.food;
+        this.ammunition = wagon.ammunition;
+        this.clothes = wagon.clothes;
+    }
+    addPart(part) {
+        this.wheels.push(wheel);
+    }
+}
+module.exports = Wagon;
