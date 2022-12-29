8a3993077f18bb01ec3ed80a644b4c4b74f8d6f9
Amber Shipley <ashipley2@me.com>
2022-12-29 15:49:21 -0800

    Implement dog functionality and unskip tests

--- adopt-a-dog/src/person.js
+++ adopt-a-dog/src/person.js
@@ -1,10 +1,38 @@
 var Dog = require("./dog.js");
 
 class Person {
-  constructor() {
-
+  constructor(human, dog) {
+    this.name = human;
+    this.dog = dog;
+    if (this.dog) {
+      this.dog.friends = [];
+    }
+    
   }
 
+  fillFoodBowl() {
+    this.dog.hungry = false;
+  }
+
+  throwBall() {
+    if (this.dog.energyLevel >= 4) {
+      this.dog.energyLevel--;
+      return `${this.dog.name} loves playing fetch!`;
+    }
+  }
+
+  introduceNewFriends(dog) {
+    this.dog.friends.push(dog.name);
+  }
+
+  adoptAPup(dogName, dogAge) { 
+    if (!this.dog) {
+      this.dog = new Dog({ name: dogName, age: dogAge});
+    }
+    else {
+      return `You can't adopt ${dogName}. You already have ${this.dog.name}!`;
+    }
+  }
 }
 
 module.exports = Person;
