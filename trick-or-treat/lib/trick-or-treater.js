e5dd51d052ed9fbda5b25a3c810b5fb53232cad6
Amber Shipley <ashipley2@me.com>
2022-12-29 17:53:01 -0800

    Finish implementing TrickOrTreater

--- trick-or-treat/lib/trick-or-treater.js
+++ trick-or-treat/lib/trick-or-treater.js
@@ -2,15 +2,18 @@ class TrickOrTreater {
     constructor(costume, bag) {
         this.dressedUpAs = costume.style;
         this.hasCandy = false;
-        // this.bag.count = 0;
         this.bag = bag;
+        this.countCandies = 0;
     }
     putCandyInBag(candy) {
-        if (!this.bag.candies.includes(candy)) {
-            this.bag.candies.push(candy);
-            this.hasCandy = true;
-        }
-        
+        this.bag.candies.push(candy);
+        this.hasCandy = true;
+        this.countCandies++;
+        this.bag.count = this.countCandies; 
+    }
+    eat() {
+        this.countCandies--;
+        this.bag.candies.splice(0, 1);
     }
 }
 module.exports = TrickOrTreater;
